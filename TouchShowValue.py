87bd1fea1980362faa33e4a202c180fa78f2b035
JakePi <jakethomashall@gmail.com>
2019-02-05 22:43:49 +0000

    Main script modification

--- TouchShowValue.py
+++ TouchShowValue.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 import touchphat
 import scrollphathd as sphd
 from envirophat import weather
@@ -36,10 +37,40 @@ def show_light():
     touchphat.all_off()
     
 @touchphat.on_touch("C")
-def show_light():
+def show_pressure():
     sphd.clear()
     pressureValue = (' Pressure: ' + str(int(weather.pressure())) + 'hPa')
     sphd.write_string(pressureValue, brightness=0.25)
+    length = sphd.get_buffer_shape()[0] - 17
+    for x in range(length):
+        sphd.show()
+        sphd.scroll(1)
+        time.sleep(0.05)
+    time.sleep(1)
+    sphd.clear()
+    sphd.show()
+    touchphat.all_off()
+
+@touchphat.on_touch("D")
+def show_altitude():
+    sphd.clear()
+    AltitudeValue = (' Altitude: ' + str(int(weather.altitude(qnh=1018))) + 'm')
+    sphd.write_string(AltitudeValue, brightness=0.25)
+    length = sphd.get_buffer_shape()[0] - 17
+    for x in range(length):
+        sphd.show()
+        sphd.scroll(1)
+        time.sleep(0.05)
+    time.sleep(1)
+    sphd.clear()
+    sphd.show()
+    touchphat.all_off()
+
+@touchphat.on_touch("Back")
+def show_datetime():
+    sphd.clear()
+    currentDT = ('DateTime: ' + str(datetime.datetime.now()))
+    sphd.write_string(currentDT, brightness=0.25)
     length = sphd.get_buffer_shape()[0] - 17
     for x in range(length):
         sphd.show()
